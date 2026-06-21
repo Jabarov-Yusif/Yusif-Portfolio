a399c566e517ec4abb8952ebb31b03fdc5f49397
Jabarov-Yusif <cabarovyusif74@gmail.com>
2026-06-22 00:26:43 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 $('.carusel').owlCarousel({
     margin: 10,
     loop: true,
@@ -12,25 +10,72 @@ $('.carusel').owlCarousel({
             items: 1,
             nav: false
         },
-        500: {
-            items: 1,
-            nav: false
-        },
-        600: {
-            items: 1,
-            nav: false
-        },
         1000: {
             items: 2,
             nav: false
-        },
+        }
     }
 });
 
-
 $(".nav-bars").click(function () {
-    $(".nav-menu").slideToggle()
+    $(".nav-menu").slideToggle();
+});
 
-})
+$(".nav-menu a").click(function () {
+    $(".nav-menu").slideUp();
+});
+
+$(".theme-btn").click(function () {
+
+    $("body").toggleClass("dark");
+
+    if ($("body").hasClass("dark")) {
+        $(".theme-btn i")
+            .removeClass("fa-moon")
+            .addClass("fa-sun");
+    } else {
+        $(".theme-btn i")
+            .removeClass("fa-sun")
+            .addClass("fa-moon");
+    }
+
+});
+
+$(window).scroll(function () {
+
+    if ($(window).scrollTop() > 50) {
+        $("header").css({
+            padding: "10px 20px"
+        });
+    } else {
+        $("header").css({
+            padding: "20px"
+        });
+    }
+
+});
 
 
+
+
+
+$(".nav-bars").click(function(){
+
+    $(".nav-menu").addClass("active");
+    $("body").addClass("menu-open");
+
+});
+
+$(".close-menu").click(function(){
+
+    $(".nav-menu").removeClass("active");
+    $("body").removeClass("menu-open");
+
+});
+
+$(".nav-menu a").click(function(){
+
+    $(".nav-menu").removeClass("active");
+    $("body").removeClass("menu-open");
+
+});
